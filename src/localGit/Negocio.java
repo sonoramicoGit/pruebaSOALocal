ade7f5fdb65d5afcfacc6dc611aab745b2f5f922
reysoac <rromeroprogramacion@gmail.com>
2021-02-26 09:21:02 -0600

    nuevo5()

--- src/localGit/Negocio.java
+++ src/localGit/Negocio.java
@@ -24,5 +24,8 @@ public class Negocio {
 	public void nuevo3() {
 		System.out.println("Negocio.nuevo3() Comentario rama local 1");
 	}
+	public void nuevo5() {
+		System.out.println("Negocio.nuevo5() Comentario rama local 1");
+	}
 
 }
